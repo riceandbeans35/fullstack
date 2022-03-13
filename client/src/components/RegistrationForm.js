a92275e68f58df5ae2a20d3299720f4709dc02cf
jlaimc <jlaimc@vt.edu>
2022-03-13 15:20:44 -0500

    created component for RegistrationForm

---
+++ client/src/components/RegistrationForm.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+function RegistrationForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("/api/merchant/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password }),
+    });
+
+    if (response.status === 201) {
+      window.location.href = "/dashboard";
+    } else {
+      console.error("Error registering merchant");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Merchant Registration</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default RegistrationForm;

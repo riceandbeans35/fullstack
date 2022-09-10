ff614704c8071001da4dc2fd9057ca743f17ed23
jlaimc <jlaimc@vt.edu>
2022-09-10 08:30:00 -0500

    Added styles to the store list page.

--- client/src/customerpages/StoreList.js
+++ client/src/customerpages/StoreList.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import { Link, useParams } from "react-router-dom";
 import CustomerNavbar from "../components/CustomerNavbar";
+import "../styles/StoreList.css";
+import store from "../assets/store.jpeg";
 
 const StoreList = () => {
   const [merchants, setMerchants] = useState([]);
@@ -20,26 +22,25 @@ const StoreList = () => {
   return (
     <div>
       <CustomerNavbar />
-      <h2>List of Stores</h2>
-      <ul
-        style={{
-          listStyleType: "none",
-          padding: 0,
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
+      <h2 className="center">Atlas Stores</h2>
+      <div className="card-container">
         {merchants.map((merchant) => (
-          <ul key={merchant.id}>
+          <div className="card" key={merchant.id}>
             <Link
               to={`/selectedstore/${merchant.id}/${customerId.id}`}
+              className="card-link"
               key={merchant.id}
             >
-              {merchant.store}
+              <img src={store} alt={merchant.store} className="card-image" />
+              <div className="card-description">
+                <p>
+                  <strong>{merchant.store}</strong>
+                </p>
+              </div>
             </Link>
-          </ul>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
